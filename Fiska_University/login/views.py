912fd92391d308eb09ff4d5610d1f55e69ef3bce
Fiskaa <dominic.naufal11@gmail.com>
2023-05-28 10:11:45 +0800

    Menambahkan library redirect di sign-up views.login

--- Fiska_University/login/views.py
+++ Fiska_University/login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
